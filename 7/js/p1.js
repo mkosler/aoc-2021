ee9aabbd80dc78f03606b85946be5e88f9d02d5c
Michael Kosler <michaelkosler@gmail.com>
2021-12-07 23:11:31 -0800

    Day 7 JS

---
+++ 7/js/p1.js
@@ -0,0 +1,38 @@
+const readline = require('readline');
+
+let positions = null;
+
+const totalCost = (p, i) => {
+  let total = 0;
+
+  for (const [k, v] of p.entries()) {
+    total += Math.abs(k - i) * v;
+  }
+
+  return total;
+};
+
+const rl = readline.createInterface({
+  input: process.stdin
+});
+
+rl.on('line', (l) => {
+  const pos = l.split(',').map(x => Number(x));
+  const length = Math.max(...pos) + 1;
+
+  positions = new Array(length).fill(0);
+
+  for (const n of pos) {
+    positions[n]++;
+  }
+});
+
+rl.on('close', () => {
+  let costs = [];
+
+  for (let i = 0; i < positions.length; i++) {
+    costs[i] = totalCost(positions, i);
+  }
+
+  console.log(`Position ${costs.indexOf(Math.min(...costs))} costs ${Math.min(...costs)}`);
+});
